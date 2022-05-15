57d5227799e3e9d82e8dbafc28110b8aaf9c3a92
NakimuliSusan <nakimulimarysusan@gmail.com>
2022-05-15 17:46:49 +0300

    calculator-app

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var btnAdd : Button
+    lateinit var btnSubtract : Button
+    lateinit var  btnMultiply : Button
+    lateinit var btnModulus : Button
+    lateinit var etNumber1 : EditText
+    lateinit var etNumber2 : EditText
+    lateinit var tvShow : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnAdd = findViewById(R.id.btnAdd)
+        btnSubtract = findViewById(R.id.btnSubtract)
+        btnMultiply = findViewById(R.id.btnMultiply)
+        btnModulus = findViewById(R.id.btnModulus)
+        etNumber1 = findViewById(R.id.etNumber1)
+        etNumber2 = findViewById(R.id.etNumber2)
+        tvShow = findViewById(R.id.tvShow)
+
+
+        btnAdd.setOnClickListener {
+           tvShow .text = ""
+           val int_Number1 = etNumber2.text.toString()
+           val int_Number2 = etNumber1.text.toString()
+
+            if(int_Number1.isBlank()){
+                etNumber2.setError("Input number")
+                return@setOnClickListener
+            }
+            else if(int_Number2.isBlank()){
+                etNumber1.setError("Input number")
+                return@setOnClickListener
+            }
+            add(int_Number1.toInt(),int_Number2.toInt())
+        }
+        btnSubtract.setOnClickListener {
+            tvShow .text = ""
+            val int_Number1 = etNumber2.text.toString()
+            val int_Number2 = etNumber1.text.toString()
+
+            if(int_Number1.isBlank()){
+                etNumber2.setError("Input number")
+                return@setOnClickListener
+            }
+            else if(int_Number2.isBlank()){
+                etNumber1.setError("Input number")
+                return@setOnClickListener
+            }
+            subtract(int_Number1.toInt(),int_Number2.toInt())
+        }
+        btnMultiply.setOnClickListener {
+            tvShow.text = ""
+            val int_Number1 = etNumber2.text.toString()
+            val int_Number2 = etNumber1.text.toString()
+
+            if (int_Number1.isBlank()) {
+                etNumber2.setError("Input number")
+                return@setOnClickListener
+            } else if (int_Number2.isBlank()) {
+                etNumber1.setError("Input number")
+                return@setOnClickListener
+            }
+
+            multiply(int_Number1.toInt(),int_Number2.toInt())
+        }
+        btnModulus.setOnClickListener {
+            tvShow.text = ""
+            val int_Number1 = etNumber2.text.toString()
+            val int_Number2 = etNumber1.text.toString()
+
+            if (int_Number1.isBlank()) {
+                etNumber2.setError("Input number")
+                return@setOnClickListener
+            } else if (int_Number2.isBlank()) {
+                etNumber1.setError("Input number")
+                return@setOnClickListener
+            }
+            modulus(int_Number1.toInt(),int_Number2.toInt())
+
+    }
+    }
+    fun add (int_Number1:Int, int_Number2: Int) {
+        val sum = int_Number1 + int_Number2
+        tvShow.text = sum.toString()
+    }
+    fun subtract (int_Number1: Int,int_Number2: Int){
+        val difference = int_Number1 - int_Number2
+        tvShow.text = difference.toString()
+    }
+    fun multiply (int_Number1: Int,int_Number2: Int) {
+        val result = int_Number1 * int_Number2
+        tvShow.text = result.toString()
+    }
+    fun modulus (int_Number1: Int,int_Number2: Int) {
+        val outcome = int_Number1 % int_Number2
+        tvShow.text = outcome.toString()
+    }
+}
